478edbed7b58324e9f016c41cbee9fb0ec888aa5
Xiangyu Huang <129352874+jehrenstonemann@users.noreply.github.com>
2024-01-30 21:17:18 -0600

    Add files via upload

---
+++ P4/cache1D.c
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        cache1D.c
+// This File:        cache1D.c
+// Other Files:      
+// Semester:         CS 354 Lecture 002 Fall 2023
+// Grade Group:      gg08  (See canvas.wisc.edu/groups for your gg#)
+// Instructor:       deppeler
+// 
+// Author:           Xiangyu Huang
+// Email:            xhuang438@wisc.edu
+// CS Login:         xiangyuh
+//
+///////////////////////////  WORK LOG  //////////////////////////////
+//  Document your work sessions on your copy http://tiny.cc/work-log
+//  Download and submit a pdf of your work log for each project.
+/////////////////////////// OTHER SOURCES OF HELP ////////////////////////////// 
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of 
+//                   of any information you find.
+// 
+// AI chats:         save a transcript and submit with project.
+//////////////////////////// 80 columns wide ///////////////////////////////////
+
+int arr[100000];
+int main(){
+	for (int i = 0; i < 100000; i++){
+		arr[i] = i;
+	}
+	return 0;
+}
---
+++ P4/cache2Dclash.c
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        cache2Dclash.c
+// This File:        cache2Dclash.c
+// Other Files:      
+// Semester:         CS 354 Lecture 002 Fall 2023
+// Grade Group:      gg08  (See canvas.wisc.edu/groups for your gg#)
+// Instructor:       deppeler
+// 
+// Author:           Xiangyu Huang
+// Email:            xhuang438@wisc.edu
+// CS Login:         xiangyuh
+//
+///////////////////////////  WORK LOG  //////////////////////////////
+//  Document your work sessions on your copy http://tiny.cc/work-log
+//  Download and submit a pdf of your work log for each project.
+/////////////////////////// OTHER SOURCES OF HELP ////////////////////////////// 
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of 
+//                   of any information you find.
+// 
+// AI chats:         save a transcript and submit with project.
+//////////////////////////// 80 columns wide ///////////////////////////////////
+
+int arr2D[128][8];
+
+int main(){
+	for (int iteration = 0; iteration < 100; iteration++){
+		for (int row = 0; row < 128;){
+			for(int col = 0; col < 8; col++){
+				arr2D[row][col] = iteration + row + col;
+			}
+			row += 64;
+		}
+	}
+	return 0;
+}
